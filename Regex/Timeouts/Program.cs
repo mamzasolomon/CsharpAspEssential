2d2668cd7f18a3dfb8541b5cf201f2d7533e7e9c
Mamza <mamzasolo@gmail.com>
2025-10-21 10:18:11 +0100

    init

--- Regex/Timeouts/Program.cs
+++ Regex/Timeouts/Program.cs
@@ -3,13 +3,14 @@
 using System.Text.RegularExpressions;
 using System.Diagnostics;
 
+const int MAX_REGEX_TIME = 1000;
 const string thestr = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 
 // Use a Stopwatch to show elapsed time
 Stopwatch sw;
 
 // TODO: Use a Timeout value when executing RegEx to guard against bad input
-
+TimeSpan Timeout = TimeSpan.FromMilliseconds(MAX_REGEX_TIME);
 
 // Run the expression and output the result
 try {
@@ -22,6 +23,6 @@ try {
         Console.WriteLine($"'{match.Value}' found at position {match.Index}");
     }
 }
-catch (Exception e) {
-    Console.WriteLine(e);
+catch (RegexMatchTimeoutException e) {
+    Console.WriteLine($"The Regex took too long to execute! {e.MatchTimeout}");
 }
